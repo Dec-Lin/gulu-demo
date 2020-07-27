fc53bcf297344f8607e8102840a7b61a5a66118a
Dec <zl@zl1998.com>
2020-07-27 18:55:14 +0800

    完成 row 和 col 的雏形

---
+++ src/col.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="col" :class="[`col-${span}`]">
+        <slot></slot>
+    </div>
+</template>
+
+
+<script>
+    export default{
+        name:'GuluCol',
+        props:{
+            span:{
+                type:[Number,String]
+            }
+        }
+    }
+</script>
+
+<style scoped lang="scss">
+    .col {
+        width: 50%;
+        height: 100px;
+        background: blue;
+        border: 1px solid black;
+
+       $class-prefix: col-;   /*申明一个class前缀*/
+        @for $n from 1 through 24 {   /*for循环，从1到24*/
+            &.#{$class-prefix}#{$n} {  /*前缀加上数字  " #{$n} " 相当于js的插值语法*/
+                width: ($n / 24) * 100%
+            }
+        }
+    }
+</style>
---
+++ src/row.vue
@@ -0,0 +1,15 @@
+<template>
+<div class="row">
+    <slot></slot>
+</div>
+</template>
+
+<style scoped lang="scss">
+    .row{
+        display: flex;
+    }
+</style>
+
+<scrtipt>
+
+</scrtipt>
