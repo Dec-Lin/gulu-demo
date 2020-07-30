26105e247e16fff55757e7f7600ab2421279fcfe
Dec <zl@zl1998.com>
2020-07-30 13:13:15 +0800

    稍微重构了一下..

--- src/col.vue
+++ src/col.vue
@@ -21,27 +21,42 @@
         props: {
             span: {type: [Number, String]},
             offset: {type: [Number, String]},
-            ipad: {type: Object,validator},
+            ipad: {type: Object, validator},
             narrowPc: {type: Object, validator},
             pc: {type: Object, validator},
-            widePc: {type: Object,validator }
+            widePc: {type: Object, validator}
         },
         data() {
             return {
                 gutter: 0
             }
         },
+        methods:{
+            createClasses(obj, str = ''){
+                if (!obj) {
+                    return []
+                }
+                let array = []
+                if (obj.span) {
+                    array.push(`col-${str}${obj.span}`)
+                }
+                if (obj.offset) {
+                    array.push(`offset-${str}${obj.offset}`)
+                }
+                return array
+            }
+
+        },
         computed: {
             colClass() {
                 let {span, offset, ipad, narrowPc, pc, widePc} = this
-
+                let createClasses = this.createClasses
                 return [
-                    span && `col-${span}`,
-                    offset && `offset-${offset}`,
-                    ...(ipad ? [`col-ipad-${ipad.span}`] : []),
-                    ...(narrowPc ? [`col-narrow-pc-${narrowPc.span}`] : []),
-                    ...(pc ? [`col-pc-${pc.span}`] : []),
-                    ...(widePc ? [`col-wide-pc-${widePc.span}`] : []),
+                    ...createClasses({span, offset}),
+                    ...createClasses(ipad, 'ipad-'),
+                    ...createClasses(narrowPc, 'narrow-pc-'),
+                    ...createClasses(pc, 'pc-'),
+                    ...createClasses(widePc, 'wide-pc-'),
                 ]
             },
             colStyle() {
@@ -72,7 +87,7 @@
             }
         }
 
-        @media (min-width: 577px){
+        @media (min-width: 577px) {
             $class-prefix: col-ipad-;
             @for $n from 1 through 24 {
                 &.#{$class-prefix}#{$n} {
@@ -87,7 +102,7 @@
             }
         }
 
-        @media (min-width: 769px){
+        @media (min-width: 769px) {
             $class-prefix: col-narrow-pc-;
             @for $n from 1 through 24 {
                 &.#{$class-prefix}#{$n} {
@@ -101,7 +116,7 @@
                 }
             }
         }
-        @media (min-width: 993px){
+        @media (min-width: 993px) {
             $class-prefix: col-pc-;
             @for $n from 1 through 24 {
                 &.#{$class-prefix}#{$n} {
